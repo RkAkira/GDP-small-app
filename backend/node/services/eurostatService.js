e84ddfbc35aba5cdbdc1f8053ad0f4c33aa9b5d6
ary.mothieu <aryh.mothieu@gmail.com>
2025-12-01 14:35:03 +0100

    Update fastAPI management

--- backend/node/services/eurostatService.js
+++ backend/node/services/eurostatService.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const PYTHON_API = process.env.PYTHON_API || "http://localhost:5000"
+const PYTHON_API = process.env.PYTHON_API || "http://localhost:8000"
 
 async function getEurostatGDP(){
     const response = await axios.get(`${PYTHON_API}/eurostat/gdp`);
