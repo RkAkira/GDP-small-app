dadf6cd751963662127ff04bc419a6a2fb53a913
ary.mothieu <aryh.mothieu@gmail.com>
2025-12-05 14:52:30 +0100

    Create Composefile + inject env variable in front end

---
+++ frontend/gdp-app/public/env.js
@@ -0,0 +1,3 @@
+(function (window) {
+  window["API_URL"] = "http://localhost:3000";
+})(this);
---
+++ frontend/gdp-app/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiUrl: (window as any)["API_URL"] || "http://localhost:3000",
+};
